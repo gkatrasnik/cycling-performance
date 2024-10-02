ef7ac589a445de94187d87ab449a18e4cd98e79b
gkatrasnikmxp <gkatrasnik@marinexchange.com>
2024-10-02 16:22:23 +0200

    Adeded newton mehod to calculate from power to time

--- CyclingPerformance.Web/Services/CalculationService.cs
+++ CyclingPerformance.Web/Services/CalculationService.cs
@@ -1,8 +1,8 @@
 ﻿using CyclingPerformance.Web.Constants;
 
 namespace CyclingPerformance.Web.Services
-{    
-    public class CalculationService: ICalculationService
+{
+    public class CalculationService : ICalculationService
     {
         public double CalculateAirDensity(double airTemperature)
         {
@@ -19,15 +19,55 @@ namespace CyclingPerformance.Web.Services
             return (altitudeGain / distance) * (massOfBike + massOfRider) * Physics.g;
         }
 
-        public double CalculateVelocity(double power, double totalForce, double airDensity)
+        public double CalculateAirResistance(double velocity, double airDensity)
         {
-            return Math.Pow((power + totalForce) / (Physics.Cd * Physics.A * 0.5 * airDensity), 0.5);
+            return CalculateAeroDragCoefficient(airDensity) * Math.Pow(velocity, 2);
         }
 
-        public double CalculatePower(double velocity, double rollingResistance, double slopePullForce, double airDensity)
+        public double CalculateVelocity(double distance, double timeInMinutes)
         {
-            return (Physics.Cd * Physics.A * 0.5 * airDensity * Math.Pow(velocity, 3)) + rollingResistance + slopePullForce;
+            return distance / (timeInMinutes * 60); 
         }
-    }    
+
+        public double CalculateTotalPower(double velocity, double rollingResistance, double slopeForce, double airResistance)
+        {
+            return velocity * (rollingResistance + slopeForce + airResistance);
+        }
+
+        public double CalculateRequiredPower(double distance, double time, double rollingResistance, double slopeForce, double airDensity)
+        {
+            double velocity = CalculateVelocity(distance, time);
+            double airResistance = CalculateAirResistance(velocity, airDensity);
+            return CalculateTotalPower(velocity, rollingResistance, slopeForce, airResistance) / Physics.DrivetrainEfficiency;
+        }
+
+        public double CalculateAeroDragCoefficient(double airDensity)
+        {
+            return Physics.Cd * Physics.A * 0.5 * airDensity;
+        }
+
+        public double NewtonMethod(double aero, double tr, double slopeForce, double power)
+        {
+            double vel = 20; // Initial guess for velocity in m/s
+            const int MAX = 10; // Max iterations
+            const double TOL = 0.05; // Tolerance for convergence
+
+            double adjustedPower = power * Physics.DrivetrainEfficiency; // Adjust power by drivetrain efficiency
+
+            for (int i = 0; i < MAX; i++)
+            {
+                double f = vel * (aero * vel * vel + tr + slopeForce) - adjustedPower;
+                double fp = aero * 3.0 * vel * vel + tr + slopeForce;
+
+                double vNew = vel - f / fp;
+
+                if (Math.Abs(vNew - vel) < TOL)
+                    return vNew;
+
+                vel = vNew;
+            }
+
+            return 0.0; // Failed to converge
+        }
+    }
 }
-    
--- CyclingPerformance.Web/Services/ICalculationService.cs
+++ CyclingPerformance.Web/Services/ICalculationService.cs
@@ -5,7 +5,12 @@
         double CalculateAirDensity(double airTemperature);
         double CalculateRollingResistance(double massOfBike, double massOfRider);
         double CalculateSlopePullForce(double altitudeGain, double distance, double massOfBike, double massOfRider);
-        double CalculateVelocity(double power, double totalForce, double airDensity);
-        double CalculatePower(double velocity, double rollingResistance, double slopePullForce, double airDensity);
+        double CalculateAirResistance(double velocity, double airDensity);
+        double CalculateVelocity(double distance, double timeInMinutes);
+        double CalculateTotalPower(double velocity, double rollingResistance, double slopeForce, double airResistance);
+        double CalculateRequiredPower(double distance, double time, double rollingResistance, double slopeForce, double airDensity);
+        double CalculateAeroDragCoefficient(double airDensity);
+        double NewtonMethod(double cda, double tr, double tran, double power);
+
     }
 }
