a7e0c4739ebf3830c1185be8bb90d7746699a3c8
gkatrasnikmxp <gkatrasnik@marinexchange.com>
2024-10-02 16:41:36 +0200

    Force from validation on calculate, temperature in celsius

--- CyclingPerformance.Web/Models/ClimbPowerTimeModel.cs
+++ CyclingPerformance.Web/Models/ClimbPowerTimeModel.cs
@@ -2,12 +2,15 @@
 {
     public class ClimbPowerTimeModel
     {
-        public double? Power { get; set; } // Power in Watts
-        public double? Time { get; set; } // Time in minutes
-        public double MassOfRider { get; set; } // Mass of rider in kg
-        public double MassOfBike { get; set; } // Mass of bicycle in kg
-        public double AirTemperature { get; set; } // Air temperature in K
-        public double Distance { get; set; } // Distance in meters
-        public double AltitudeGain { get; set; } // Altitude gain in meters
+        public double? Power { get; set; }
+        public double? Time { get; set; }
+        public double MassOfRider { get; set; }
+        public double MassOfBike { get; set; }
+        public double AirTemperatureCelsius { get; set; }
+        public double Distance { get; set; }
+        public double AltitudeGain { get; set; }
+
+        public double AirTemperatureKelvin => AirTemperatureCelsius + 273.15;
+
     }
 }
